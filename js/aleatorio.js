bea90dc999dd76a45d2b1b28ac7a58005366608f
Javier Jimenez <javier.sniguel@gmail.com>
2023-11-26 23:21:41 -0300

    Se creo la rama de js, se agregaron las variables de numero aleatorio y del inicio de juego

---
+++ js/aleatorio.js
@@ -0,0 +1,7 @@
+let numeroMagico;
+let juegoIniciado = false;
+
+// Función para generar un número aleatorio entre 1 y 100
+function generarNumeroMagico() {
+    numeroMagico = Math.floor(Math.random() * 100) + 1;
+}
